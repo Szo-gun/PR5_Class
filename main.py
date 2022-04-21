f0e4d98c9fb653d3a24e88c9fdf50e402d124db8
Piotr Cianciara <piotr.cianciara@comatec.fi>
2022-04-21 14:08:13 +0200

    Add BaseContact and BusinessContact Classes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from cgi import test
 from re import I
+from tracemalloc import start
 from turtle import color
 from faker import Faker
 fake = Faker()
@@ -14,6 +15,7 @@ class event:
         #self.firma = firma
         #self.stanowisko = stanowisko
         self.mail = mail
+
         #Variables:
         self._imie_len = 0
         self._nazwisko_len = 0 
@@ -24,7 +26,7 @@ class event:
     def imie_len(self):
         return self._imie_len
     def imie_len(self):
-         print(f"Imię {person.imie} ma {len(person.imie)} znaków")
+        print(f"Imię {person.imie} ma {len(person.imie)} znaków")
 
     def nazwisko_len(self):
         return self._nazwisko_len
@@ -32,24 +34,59 @@ class event:
         print(f"Nazwisko {person.nazwisko} ma {len(person.nazwisko)} znaków"  + "\n")
 
     def contact(self):
-        print(f"Kontaktuję się z: {person}")
+        print(f"Kontaktuję się z: {person.imie} {person.nazwisko}")
 
-def dane(name):
-    #return(name.imie, name.nazwisko, name.firma, name.stanowisko, name.mail)
-    return(name.imie, name.nazwisko, name.mail)
 
-# Pętla generująca 5 losowych wizytówek
-for i in range(0,5):
-    #person = event(imie=fake.first_name(), nazwisko=fake.last_name(), firma=fake.company(), stanowisko=fake.job(), mail=fake.email() + "\n")
-    person = event(imie=fake.first_name(), nazwisko=fake.last_name(), mail=fake.email())
-    dane(person) 
-    globals()["Nowy%s" % i] = person
-    event.contact(person)
-    event.imie_len(person.imie)
-    event.nazwisko_len(person.imie)
+#Rozszerzenie klasy Event na klasę Base i Business
 
-# Użycie metody contact - wyświetlającej zapis: "Kontaktuję się z {}"
+class BaseContact(event):
+   def __init__(self, telefon_prywatny, *args, **kwargs):
+       super().__init__(*args, **kwargs)
+       self.telefon_prywatny = telefon_prywatny
+       
+   def contact_PR(self):
+       print(f"Kontaktuję się z: {person.imie} {person.nazwisko} i dzwonię pod: {person.telefon_prywatny}")
 
+def dane_PR(name):
+    return(name.imie, name.nazwisko, name.mail, name.telefon_prywatny)
+
+#JAK ZROBIĆ OPCJONALNY?!?!?!?!
+class BusinessContact(event):
+    def __init__ (self, telefon_biznes, *args, **kwargs):
+    #def __init__ (self, stanowisko, firma, telefon_biznes, *args, **kwargs):
+       super().__init__(*args, **kwargs)
+       #self.stanowisko = stanowisko
+       #self.firma = firma
+       self.telefon_biznes = telefon_biznes
+    
+    def contact_BU(self):
+        print(f"Kontaktuję się z {person.imie} {person.nazwisko} i dzwonię pod: {person.telefon_biznes}")
+
+def dane_BU(name):
+    return(name.imie, name.nazwisko, name.mail, name.telefon_biznes)
+
+decision = input("Podaj czy chcesz wyświetlić kontakty prywatne [P], czy biznesowe [B]: ")
+wybor = decision.upper()
+if wybor == 'P':
+# Pętla generująca 5 losowych wizytówek prywatnych
+    for i in range(0,5):
+        person = BaseContact(imie=fake.first_name(), nazwisko=fake.last_name(), mail=fake.email(),telefon_prywatny = fake.phone_number())
+        dane_PR(person) 
+        globals()["Nowy%s" % i] = person
+        BaseContact.contact_PR(person)
+        event.imie_len(person.imie)
+        event.nazwisko_len(person.imie)
+elif wybor == 'B':
+# Pętla generująca 5 losowych wizytówek biznesowych
+    for i in range(0,5):
+        person = BusinessContact(imie=fake.first_name(), nazwisko=fake.last_name(), mail=fake.email(),telefon_biznes = fake.phone_number())
+        dane_BU(person) 
+        globals()["Nowy%s" % i] = person
+        BusinessContact.contact_BU(person)
+        event.imie_len(person.imie)
+        event.nazwisko_len(person.imie)
+else:
+    print("Nie ma takiej wizytówki")
 
 # Lista z wygenerowanymi kontaktami    
 Wszyscy = [Nowy0, Nowy1, Nowy2, Nowy3, Nowy4]
