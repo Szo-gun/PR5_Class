073c1aa4275ba015b3bdb078d61e3ea88c448d08
Piotr Cianciara <piotr.cianciara@comatec.fi>
2022-04-21 15:22:19 +0200

    Change arguments to optional in BusinessContact Class

--- main.py
+++ main.py
@@ -1,64 +1,44 @@
-from cgi import test
-from re import I
-from tracemalloc import start
-from turtle import color
 from faker import Faker
 fake = Faker()
 
-###JAK ZROBIĆ ABY ARGUMENTY BYŁY OPCJONALNE?!
-
 class event:
     def __init__(self, imie, nazwisko, mail):
-    #def __init__(self, imie, nazwisko, firma, stanowisko, mail):
         self.imie =  imie
         self.nazwisko = nazwisko
-        #self.firma = firma
-        #self.stanowisko = stanowisko
         self.mail = mail
 
         #Variables:
         self._imie_len = 0
         self._nazwisko_len = 0 
     def __repr__(self):
-       #return f"{self.imie} {self.nazwisko}, {self.firma}, {self.stanowisko}, {self.mail}"
         return f"{self.imie} {self.nazwisko}, {self.mail}"
-
     def imie_len(self):
         return self._imie_len
     def imie_len(self):
         print(f"Imię {person.imie} ma {len(person.imie)} znaków")
-
     def nazwisko_len(self):
         return self._nazwisko_len
     def nazwisko_len(self):
         print(f"Nazwisko {person.nazwisko} ma {len(person.nazwisko)} znaków"  + "\n")
 
-    def contact(self):
-        print(f"Kontaktuję się z: {person.imie} {person.nazwisko}")
-
-
 #Rozszerzenie klasy Event na klasę Base i Business
 
 class BaseContact(event):
-   def __init__(self, telefon_prywatny, *args, **kwargs):
+    def __init__(self, telefon_prywatny, *args, **kwargs):
        super().__init__(*args, **kwargs)
        self.telefon_prywatny = telefon_prywatny
-       
-   def contact_PR(self):
+    def contact_PR(self):
        print(f"Kontaktuję się z: {person.imie} {person.nazwisko} i dzwonię pod: {person.telefon_prywatny}")
 
 def dane_PR(name):
     return(name.imie, name.nazwisko, name.mail, name.telefon_prywatny)
 
-#JAK ZROBIĆ OPCJONALNY?!?!?!?!
 class BusinessContact(event):
-    def __init__ (self, telefon_biznes, *args, **kwargs):
-    #def __init__ (self, stanowisko, firma, telefon_biznes, *args, **kwargs):
+    def __init__ (self, stanowisko = None, firma = None, telefon_biznes = None, *args, **kwargs):
        super().__init__(*args, **kwargs)
-       #self.stanowisko = stanowisko
-       #self.firma = firma
+       self.stanowisko = stanowisko
+       self.firma = firma
        self.telefon_biznes = telefon_biznes
-    
     def contact_BU(self):
         print(f"Kontaktuję się z {person.imie} {person.nazwisko} i dzwonię pod: {person.telefon_biznes}")
 
